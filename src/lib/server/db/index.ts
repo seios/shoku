fc631fba600eba7a92a7900a08bbfe42eeb6ba92
seios <seios3566@gmail.com>
2025-02-14 07:00:48 +0900

    fix DB handling

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL, { prepare: false });
+import { DATABASE_URL } from '$env/static/private';
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = postgres(DATABASE_URL, { prepare: false });
 export const db = drizzle(client);
