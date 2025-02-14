bc58dd18d296230f155fec48e7e06b2fcf52967f
seios <seios3566@gmail.com>
2025-02-15 07:31:25 +0900

    test env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,30 +1,44 @@
+import { env } from '$env/dynamic/private';
 import { redirect, type Handle } from '@sveltejs/kit';
-import * as auth from '$lib/server/auth.js';
+// import * as auth from '$lib/server/auth.ts';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	// Get the root directory name
 	const rootDirName = event.route.id?.split('/')[1] || '';
 	const protectedDirName = '(authed)';
 
-	const sessionToken = event.cookies.get(auth.sessionCookieName);
-	if (!sessionToken) {
-		event.locals.user = null;
-		event.locals.session = null;
-		if (rootDirName === protectedDirName) {
-			return redirect(301, '/login');
-		}
-		return resolve(event);
+	if (rootDirName === protectedDirName) {
+		const localSecretVar = env?.SECRETVAR;
+		const cfSecretVar = event?.platform?.env.SECRETVAR;
+		console.log({localSecretVar, cfSecretVar});
+		return redirect(301, '/login');
 	}
 
-	const { session, user } = await auth.validateSessionToken(sessionToken);
-	if (session) {
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-	} else {
-		auth.deleteSessionTokenCookie(event);
-	}
 
-	event.locals.user = user;
-	event.locals.session = session;
+
+	// Get the root directory name
+	// const rootDirName = event.route.id?.split('/')[1] || '';
+	// const protectedDirName = '(authed)';
+
+	// const sessionToken = event.cookies.get(auth.sessionCookieName);
+	// if (!sessionToken) {
+	// 	event.locals.user = null;
+	// 	event.locals.session = null;
+	// 	if (rootDirName === protectedDirName) {
+	// 		return redirect(301, '/login');
+	// 	}
+	// 	return resolve(event);
+	// }
+
+	// const { session, user } = await auth.validateSessionToken(sessionToken);
+	// if (session) {
+	// 	auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+	// } else {
+	// 	auth.deleteSessionTokenCookie(event);
+	// }
+
+	// event.locals.user = user;
+	// event.locals.session = session;
 
 	return resolve(event);
 };
